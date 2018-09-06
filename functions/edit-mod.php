a5672015f703276885f1921ffd6a3ab139f3dfff
TheGameSpider <thegamespider@gmail.com>
2018-09-06 20:32:33 +0200

    edit mod

---
+++ functions/edit-mod.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+require("dbconnect.php");
+if(!$_SESSION['user']||$_SESSION['user']=="") {
+	die("Must be logged in!");
+}
+if(empty($_GET['id'])){
+	die("Mod not specified.");
+}
+$result = mysqli_query($conn, "SELECT * FROM `mods` WHERE `id` = ".$_GET['id']);
+$mod = mysqli_fetch_row($result);
+if($mod['name']!==$_POST['name']){
+	mysqli_query($conn, "UPDATE `mods` SET `name` = '".$_POST['name']."' WHERE `id` = ".$_GET['id']);
+}
+if($mod['pretty_name']!==$_POST['pretty_name']){
+	mysqli_query($conn, "UPDATE `mods` SET `pretty_name` = '".$_POST['pretty_name']."' WHERE `id` = ".$_GET['id']);
+}
+if($mod['link']!==$_POST['link']){
+	mysqli_query($conn, "UPDATE `mods` SET `link` = '".$_POST['link']."' WHERE `id` = ".$_GET['id']);
+}
+if($mod['author']!==$_POST['author']){
+	mysqli_query($conn, "UPDATE `mods` SET `author` = '".$_POST['author']."' WHERE `id` = ".$_GET['id']);
+}
+if($mod['donlink']!==$_POST['donlink']){
+	mysqli_query($conn, "UPDATE `mods` SET `donlink` = '".$_POST['donlink']."' WHERE `id` = ".$_GET['id']);
+}
+if($mod['version']!==$_POST['version']){
+	mysqli_query($conn, "UPDATE `mods` SET `version` = '".$_POST['version']."' WHERE `id` = ".$_GET['id']);
+}
+if($mod['description']!==$_POST['description']){
+	mysqli_query($conn, "UPDATE `mods` SET `description` = '".$_POST['description']."' WHERE `id` = ".$_GET['id']);
+}
+if($mod['mcversion']!==$_POST['version']){
+	mysqli_query($conn, "UPDATE `mods` SET `mcversion` = '".$_POST['mcversion']."' WHERE `id` = ".$_GET['id']);
+}
+header("Location: /mod?id=".$_GET['id']);
