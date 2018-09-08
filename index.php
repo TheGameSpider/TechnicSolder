b8ecb4f131271f8bcde54226c1426a65131a946d
TheGameSpider <thegamespider@gmail.com>
2018-09-08 14:50:13 +0200

    accept zip files and added icon and title

--- index.php
+++ index.php
@@ -55,6 +55,8 @@ if(isset($_GET['logout'])){
 ?>
 <html>
 	<head>
+		<link rel="shortcut icon" href="./resources/wrenchIcon.svg" />
+		<title>Technic Solder</title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -1086,7 +1088,7 @@ if(isset($_GET['logout'])){
 									<i class="fas fa-upload fa-4x"></i>
 								</div>									
 							</center>
-							<input type="file" name="fiels" multiple/>
+							<input type="file" name="fiels" multiple accept=".zip" />
 						</div>
 					</form>
 				</div>
