ee61a833da4188d99f760d135a82d30d97a9d875
TheGameSpider <thegamespider@gmail.com>
2018-09-08 21:54:25 +0200

    0.0.2.4

--- api/index.php
+++ api/index.php
@@ -21,7 +21,7 @@ function uri($url, $uri) {
     return (substr($url, -$length) === $uri);
 }
 if(uri($url,"api/")){
-	print '{"api":"TechnicSolder","version":"v0.0.2.3","stream":"Beta"}';
+	print '{"api":"TechnicSolder","version":"v0.0.2.4","stream":"Beta"}';
 	exit();
 } 
 if(uri($url,"api/verify")){
