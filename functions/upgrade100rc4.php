314493d34a93ccc5b86987ecdf32c2e9d147d75f
TheGameSpider <thegamespider@gmail.com>
2018-12-29 19:32:20 +0100

    Update upgrade100rc4.php

--- functions/upgrade100rc4.php
+++ functions/upgrade100rc4.php
@@ -9,6 +9,8 @@ mysqli_query($conn, "ALTER TABLE `builds` ADD COLUMN `public` TINYINT(1);");
 mysqli_query($conn, "ALTER TABLE `builds` ADD COLUMN `clients` LONGTEXT;");
 mysqli_query($conn, "ALTER TABLE `modpacks` ADD COLUMN `public` TINYINT(1);");
 mysqli_query($conn, "ALTER TABLE `modpacks` ADD COLUMN `clients` LONGTEXT;");
+mysqli_query($conn, "UPDATE `modpacks` SET `public` = 1;");
+mysqli_query($conn, "UPDATE `builds` SET `public` = 1;");
 $sql = "
 CREATE TABLE clients (
 id int(8) AUTO_INCREMENT PRIMARY KEY,
