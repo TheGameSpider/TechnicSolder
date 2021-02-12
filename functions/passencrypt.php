efe7bb669b227a1cfbb44514440b204c8590eb17
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2021-02-11 22:00:20 -0500

    Fix issue with password encryption executing twice.

--- functions/passencrypt.php
+++ functions/passencrypt.php
@@ -11,6 +11,7 @@ if($_SESSION['user']!==$config['mail']) {
 //$sql = mysqli_query($conn,"UPDATE `users` SET `display_name` = '".$_POST['display_name']."', `perms` = '".$_POST['perms']."' WHERE `name` = '".$_POST['name']."'");
 if(!isset($config['encrypted'])||$config['encrypted']==false||!isset($config['betterencryption'])||$config['betterencryption']==false) {
 	$users = mysqli_query($conn,"SELECT * FROM `users`");
+	$alreadydone = false
 	// If better encryption isnt used and passwords arent already using encryption.
 	if((!isset($config['betterencryption'])||$config['betterencryption']==false)&&(!isset($config['encrypted'])||$config['encrypted']==false)){
 		error_log("Upgrading from no encryption at all.");
@@ -19,11 +20,12 @@ if(!isset($config['encrypted'])||$config['encrypted']==false||!isset($config['be
 		}
 		$mainpass = password_hash(hash("sha256",$config['pass']."Solder.cf"), PASSWORD_DEFAULT);
 		$cf = '<?php return array( "configured" => true, "author" => "'.$config['author'].'","mail" => "'.$config['mail'].'","pass" => "'.$mainpass.'","db-host" => "'.$config['db-host'].'","db-user" => "'.$config['db-user'].'","db-name" => "'.$config['db-name'].'","db-pass" => "'.$config['db-pass'].'","host" => "'.$config['host'].'","dir" => "'.$config['dir'].'","api_key" => "'.$config['api_key'].'", "encrypted" => true, "betterencryption" => true ';
+		$alreadydone = true
 		file_put_contents("./config.php", $cf." );");
 		header("Location: ../");
 	}
 	// If better encryption isnt used, but passwords already are encrypted.
-	if((!isset($config['betterencryption'])||$config['betterencryption']==false)&&(isset($config['encrypted'])||$config['encrypted']==true)){
+	if((!isset($config['betterencryption'])||$config['betterencryption']==false)&&(isset($config['encrypted'])||$config['encrypted']==true)&&$alreadydone==false){
 		error_log("Upgrading from old encryption.");
 		while ($user = mysqli_fetch_array($users)) {
 			mysqli_query($conn,"UPDATE `users` SET `pass` = '".password_hash($user['pass'], PASSWORD_DEFAULT)."' WHERE `name` = '".$user['name']."'");
