f3d2a94bed23e74a3e3ecdc5288a042eacec4933
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2021-02-11 20:14:53 -0500

    Add fixes to security vulnerability patch.

--- functions/passencrypt.php
+++ functions/passencrypt.php
@@ -11,13 +11,26 @@ if($_SESSION['user']!==$config['mail']) {
 //$sql = mysqli_query($conn,"UPDATE `users` SET `display_name` = '".$_POST['display_name']."', `perms` = '".$_POST['perms']."' WHERE `name` = '".$_POST['name']."'");
 if(!isset($config['encrypted'])||$config['encrypted']==false||!isset($config['betterencryption'])||$config['betterencryption']==false) {
 	$users = mysqli_query($conn,"SELECT * FROM `users`");
-	while ($user = mysqli_fetch_array($users)) {
-		mysqli_query($conn,"UPDATE `users` SET `pass` = '".password_hash(hash("sha256",$user['pass']."Solder.cf"), PASSWORD_DEFAULT)."' WHERE `name` = '".$user['name']."'");
+	// If better encryption isnt used and passwords arent already using encryption.
+	if((!isset($config['betterencryption'])||$config['betterencryption']==false)&&(!isset($config['encrypted'])||$config['encrypted']==false)){
+		while ($user = mysqli_fetch_array($users)) {
+			mysqli_query($conn,"UPDATE `users` SET `pass` = '".password_hash(hash("sha256",$user['pass']."Solder.cf"), PASSWORD_DEFAULT)."' WHERE `name` = '".$user['name']."'");
+		}
+		$mainpass = password_hash(hash("sha256",$config['pass']."Solder.cf"), PASSWORD_DEFAULT);
+		$cf = '<?php return array( "configured" => true, "author" => "'.$config['author'].'","mail" => "'.$config['mail'].'","pass" => "'.$mainpass.'","db-host" => "'.$config['db-host'].'","db-user" => "'.$config['db-user'].'","db-name" => "'.$config['db-name'].'","db-pass" => "'.$config['db-pass'].'","host" => "'.$config['host'].'","dir" => "'.$config['dir'].'","api_key" => "'.$config['api_key'].'", "encrypted" => true, "betterencryption" => true ';
+		file_put_contents("./config.php", $cf." );");
+		header("Location: ../");
 	}
-	$mainpass = password_hash(hash("sha256",$config['pass']."Solder.cf"), PASSWORD_DEFAULT);
-	$cf = '<?php return array( "configured" => true, "author" => "'.$config['author'].'","mail" => "'.$config['mail'].'","pass" => "'.$mainpass.'","db-host" => "'.$config['db-host'].'","db-user" => "'.$config['db-user'].'","db-name" => "'.$config['db-name'].'","db-pass" => "'.$config['db-pass'].'","host" => "'.$config['host'].'","dir" => "'.$config['dir'].'","api_key" => "'.$config['api_key'].'", "encrypted" => true, "betterencryption" => true ';
-	file_put_contents("./config.php", $cf." );");
-	header("Location: ../");
+	// If better encryption isnt used, but passwords already are encrypted.
+	if((!isset($config['betterencryption'])||$config['betterencryption']==false)&&(isset($config['encrypted'])||$config['encrypted']==true)){
+		while ($user = mysqli_fetch_array($users)) {
+			mysqli_query($conn,"UPDATE `users` SET `pass` = '".password_hash($user['pass'], PASSWORD_DEFAULT)."' WHERE `name` = '".$user['name']."'");
+		}
+		$mainpass = password_hash($config['pass'], PASSWORD_DEFAULT);
+		$cf = '<?php return array( "configured" => true, "author" => "'.$config['author'].'","mail" => "'.$config['mail'].'","pass" => "'.$mainpass.'","db-host" => "'.$config['db-host'].'","db-user" => "'.$config['db-user'].'","db-name" => "'.$config['db-name'].'","db-pass" => "'.$config['db-pass'].'","host" => "'.$config['host'].'","dir" => "'.$config['dir'].'","api_key" => "'.$config['api_key'].'", "encrypted" => true, "betterencryption" => true ';
+		file_put_contents("./config.php", $cf." );");
+		header("Location: ../");
+	}
 } else {
 	//$pass = password_hash(hash("sha256",$_POST['pass']."Solder.cf"), PASSWORD_DEFAULT);
 	die("Passwords are already encrypted using new encryption.");
