1a022554bad60789772292d138dd8a77699b7f21
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2021-02-11 21:11:12 -0500

    Version bump on version.json

--- api/index.php
+++ api/index.php
@@ -22,7 +22,7 @@ function uri($url, $uri) {
     return (substr($url, -$length) === $uri);
 }
 if(uri($url,"api/")){
-	print '{"api":"Solder.cf","version":"v1.3.2","stream":"Release"}';
+	print '{"api":"Solder.cf","version":"v1.3.4","stream":"Release"}';
 	exit();
 } 
 if(uri($url,"api/verify")){
