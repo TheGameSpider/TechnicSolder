df5451e118e7eea03ab6eda0e235640f7f0119f8
Zander M <zander@zandercraft.ca>
2022-10-31 12:56:05 -0400

    Bring up to date with master (thanks cowpod)

--- functions/custom_forge.php
+++ functions/custom_forge.php
@@ -17,7 +17,7 @@ if (!$fileTmpLoc) {
 }
 function slugify($text) {
   $text = preg_replace('~[^\pL\d]+~u', '-', $text);
-  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+  // $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
   $text = preg_replace('~[^-\w]+~', '', $text);
   $text = trim($text, '-');
   $text = preg_replace('~-+~', '-', $text);
--- functions/forge-links.php
+++ functions/forge-links.php
@@ -1,43 +1,38 @@
 <?php
 header('Content-Type: application/json');
 require("dbconnect.php");
-$forge_data = file_get_contents("http://files.minecraftforge.net/maven/net/minecraftforge/forge/promotions.json");
+$forge_data = file_get_contents("https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json"); // can't find normal promotions.json
+$forge_link = "https://maven.minecraftforge.net/net/minecraftforge/forge"; // /1.16.5-36.1.16/forge-1.16.5-36.1.16-installer.jar
 $versions = [];
 $forges = [];
 $id = 0;
-foreach(json_decode($forge_data, true)['promos'] as $forge => $ff) {
+foreach(json_decode($forge_data, true)['promos'] as $gameVersion => $forgeVersion) { // key, value
     $id++;
-    if($forge !== "latest" & $forge !== "recommended") {
-        if(strpos($forge, "latest")) {
-            $fv = str_replace("-latest", "", $forge);
-            $fvs = $ff['version'];
-            foreach ($ff['files'] as $file) {
-                $fext=$file[0];
-                if($fext=="zip"||$fext=="jar") {
-                    $fn = str_replace("latest", $fvs, $forge);
-                    if($fv == "1.12.1"|| $fv == "1.12" || $fv == "1.11.2" || $fv == "1.11.1" || $fv == "1.11" || $fv == "1.10.2"|| $fv == "1.10" || $fv == "1.9.4" || $fv == "1.9" || $fv == "1.8.9" || $fv == "1.8.8" || $fv == "1.8" || $fv == "1.7.10" || $fv == "1.7.2" || $fv == "1.6.4" || $fv == "1.6.3" || $fv == "1.6.2" || $fv == "1.6.1" || $fv == "1.5.2" || $fv == "1.1"){
-                        $versions[$fv] = "https://files.minecraftforge.net/maven/net/minecraftforge/forge/".$fn."/forge-".$fn."-universal.".$fext;
-                    } else {
-                        $versions[$fv] = "https://files.minecraftforge.net/maven/net/minecraftforge/forge/".$fn."/forge-".$fn."-installer.".$fext;
-                    }
-                }
-            }
-            $modsq = mysqli_query($conn, "SELECT * FROM `mods` WHERE `name` = 'forge' AND `version` = '".$fvs."'");
-            if(mysqli_num_rows($modsq)==0) {
-                $forges[$fv] = array(
-                    "id" => $id,
-                    "mc" => $fv,
-                    "name" => $fvs,
-                    "link" => $versions[$fv]
-                );
-            }
+    if(strpos($gameVersion, "latest")) {
+        $gameVersion = str_replace("-latest", "", $gameVersion);
+        // 1.13 "1.13.2-25.0.219" and later don't have a universal.jar
+        if (version_compare($gameVersion, '1.13', '>=')) {
+            $suffixExt = 'installer.jar';
+        } else {
+            $suffixExt = 'universal.jar';
+        }
+        $versions[$forgeVersion] = $forge_link.'/'.$gameVersion.'-'.$forgeVersion.'/forge-'.$gameVersion.'-'.$forgeVersion.'-'.$suffixExt;
+
+        $modsq = mysqli_query($conn, "SELECT * FROM `mods` WHERE `name` = 'forge' AND `version` = '".$forgeVersion."'");
+        if(mysqli_num_rows($modsq)==0) {
+            $forges[$gameVersion] = array(
+                "id" => $id,
+                "mc" => $gameVersion,
+                "name" => $forgeVersion,
+                "link" => $versions[$forgeVersion]
+            );
         }
     }
 }
-$forges['1.7.10'] = array(
-    "id" => 99,
-    "mc" => "1.7.10",
-    "name" => "10.13.4.1614",
-    "link" => "https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.7.10-10.13.4.1614-1.7.10/forge-1.7.10-10.13.4.1614-1.7.10-universal.jar"
-);
+//$forges['1.7.10'] = array(
+//    "id" => 99,
+//    "mc" => "1.7.10",
+//    "name" => "10.13.4.1614",
+//    "link" => "https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.7.10-10.13.4.1614-1.7.10/forge-1.7.10-10.13.4.1614-1.7.10-universal.jar"
+//);
 print_r(json_encode($forges, JSON_PRETTY_PRINT));
